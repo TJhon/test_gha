e66d50ee05d3bb7ec328038cf1058c5da50bac0e
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2022-07-19 21:16:58 -0500

    update r dependencies

--- r_files/dep.r
+++ r_files/dep.r
@@ -2,9 +2,43 @@ install.packages("librarian")
 librarian::shelf(
     tidyverse
     , keras
+    , broom
+    , hdm
+    , sandwich
+    , lmtest
     , h2o
+    , glmnet
+    , randomForest
+    , rpart
+    , nnet
+    , gbm
+    , rpart.plot
+    , keras
+    , dagitty
+    , ggdag
+    , ORCI
+    , lfe
+    , ggraph
+    , DoubleML
+    , mlr3learners
+    , mlr3
+    , data.table
+    , AER
+    , xgboost
+    , zoo
+    , survival
+    , car
+    , carData
+    , h2o
+    , Rcpp
+    , regr.ranger
+    , classif.ranger
 )
-install.packages("remotes")
-remotes::install_github("IRkernel/IRkernel")
 
+install.packages("remotes")
+install.packages("dagitty")
+install.packages("ggdag")
+
+remotes::install_github("IRkernel/IRkernel")
+remotes::install_github("cran/ORCI")
 IRkernel::installspec()
